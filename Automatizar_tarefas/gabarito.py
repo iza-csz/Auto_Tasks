7e8561c2d87dd6e031a58e9cc44a01bb21956bfa
nao <izadoracibantosdesouza@gmail.com>
2026-01-13 10:57:50 -0300

    passos essenciais
    (parte do código que quase não muda conforme a tarefa executada)

---
+++ Automatizar_tarefas/gabarito.py
@@ -0,0 +1,31 @@
+import pyautogui
+import time 
+
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+
+# abrir windows, clicar na barra de pesquisa, digitar edje, clicar enter, clicar na barra de pesquisa do edge, clicar na barra de pesquisa, digitar link, clicar enter
+
+pyautogui.press('win')
+time.sleep(1)
+pyautogui.write('edge')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.click(x=100, y=100)  # Click on the search bar in Edge
+time.sleep(1)
+pyautogui.write(link)
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(5)
+
+# clicar em email, digitar email, clicar em senha, digitar senha, clicar em entrar
+
+pyautogui.click(x=536, y=370)
+pyautogui.write("izadorainteligente@gmail.com")
+pyautogui.press('tab')
+pyautogui.write("jesusefiel1234@")
+pyautogui.press('tab')
+pyautogui.press('enter')
+time.sleep(5)
+
+# configurar etapas da tarefa
