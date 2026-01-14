1ad2aae14819287fbe1a2b9900a36951c723937a
nao <izadoracibantosdesouza@gmail.com>
2026-01-14 12:36:48 -0300

    aaaaa

--- Automatizar_tarefas/gabarito.py
+++ Automatizar_tarefas/gabarito.py
@@ -20,10 +20,8 @@ time.sleep(3)
 
 # clicar em email, digitar email, clicar em senha, digitar senha, clicar em entrar
 
-pyautogui.click(x=601, y=397)
-time.sleep(1)
+pyautogui.click(x=530, y=399)
 pyautogui.write("izadorainteligente@gmail.com")
-time.sleep(1)
 pyautogui.press('tab')
 pyautogui.write("jesusefiel1234@")
 pyautogui.press('tab')
@@ -38,5 +36,26 @@ print (tabela)
 
 # configurar etapas da tarefa
 for linha in tabela.index:
+    pyautogui.click (x=491, y=284)
 
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(str(codigo))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
 
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna (obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+    pyautogui.scroll (4457)
